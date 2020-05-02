b90fa4b88bc94e0c74e7fba583d6fd5085616793
bcharles <bcharles41078@gmail.com>
2020-05-01 22:34:52 -0500

    wireframe

---
+++ store.js
@@ -0,0 +1,23 @@
+const store = {
+    bookmarks: [
+        {
+            id: 'x56w',
+            title: 'Title 1',
+            rating: 3,
+            url: 'http://www.title1.com',
+            description: 'lorem ipsum dolor sit',
+            expanded: true
+        },
+        {
+            id: '6ffw',
+            title: 'Title 2',
+            rating: 5,
+            url: 'http://www.title2.com',
+            description: 'dolorum tempore deserunt',
+            expanded: false
+        }
+    ],
+    adding: false,
+    error: null,
+    filter: 0
+};
